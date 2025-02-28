d7fb498226c2b469a21dd8e2e7d5e5373cae713f
Sifiso Maye <sifmay98@gmail.com>
2025-02-28 16:53:45 +0200

    bookmarks

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
-  resources :lists, only: [:create, :index, :list, :show]
+   root "lists#index"
+  resources :lists, only: [:create, :new, :show] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
+
 end
